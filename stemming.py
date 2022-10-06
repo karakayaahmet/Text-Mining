03f4e78443d096155ceca1902aac5a41069abbdc
karakayaahmet <65498435+karakayaahmet@users.noreply.github.com>
2022-10-06 20:48:49 +0300

    stemming işlemi tamamlandı.

---
+++ stemming.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from nltk.stem import PorterStemmer
+
+st = PorterStemmer
+
+metin = """
+A Scandal in Bohemia! 01
+The Red-headed League,2
+A Case, of Identity 33
+The Boscombe Valley Mystery4
+The Five Orange Pips1
+The Man with? the Twisted Lip
+The Adventure of the Blue Carbuncle
+The Adventure of the Speckled Band
+The Adventure of the Engineer's Thumb
+The Adventure of the Noble Bachelor
+The Adventure of the Beryl Coronet
+The Adventure of the Copper Beeches"""
+
+print(metin)
+v = metin.split("\n")
+seri = pd.Series(v)
+df = pd.DataFrame(seri, columns=["hikayeler"])
+
+a = df["hikayeler"].apply(lambda x: " ".join([st.stem(i) for i in x.split()]))
+print(a)
+
