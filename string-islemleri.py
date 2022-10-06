8a8c4e2d3443985fe925d2a0c5c1463f3d815ade
karakayaahmet <65498435+karakayaahmet@users.noreply.github.com>
2022-10-06 18:38:15 +0300

    string işlemleri tamamlandı.

--- string-islemleri.py
+++ string-islemleri.py
@@ -1,5 +1,7 @@
 import enum
+from fnmatch import translate
 from traceback import print_tb
+import pandas as pd
 
 #Oluşturma Biçimlendirme
 
@@ -36,6 +38,124 @@ print("123".isnumeric)
 
 print("234".isdigit)
 
+print("a12".isalnum)
 
+#Elemanlarına ve eleman indexlerine erişmek
 
+isim = "mvk"
 
+print(isim[0:2])
+
+print(isim.index("v"))
+
+#Başlangıç ve Bitiş Karakterlerini Sorgulamak.
+
+m = "mustafa"
+
+print(m.startswith("a"))
+
+print(m.endswith("b"))
+
+#Count
+
+print(m.count("a"))
+
+#Sorted 
+
+print(sorted(m))
+
+print(*sorted(m), sep="")
+
+#Karakterleri Bölmek
+
+print(m.split())
+
+print(m.split("a"))
+
+#Büyük Küçük Harf İşlemleri
+
+print(m.lower())
+
+print(m.upper())
+
+print(m.upper().lower())
+
+print(m.isupper())
+
+print(m.islower())
+
+#Capitalize, Title, Swapcase
+
+print(m.capitalize())
+
+print(m.title())
+
+print(m.swapcase()) #Büyük olan karakterler küçük, küçük olan karakterler büyük olacak.
+
+#İstenmeyen Karakterleri Kırpmak: strip(), lstrip(), rstrip()
+
+s = " hello "
+
+print(s.strip())
+
+s = "**hello**"
+
+print(s.strip("*"))
+
+print(s.rstrip("*"))
+
+print(s.lstrip("*"))
+
+#Bölünmüş ya da Bölük Olan İfadelerin Birleştirilmesi
+
+okul = "Firat Üniversitesi Yazilim Muhendisligi"
+
+ayrik = okul.split()
+
+print(ayrik)
+
+joiner = " "
+
+print(joiner.join(ayrik))
+
+yildiz = "**"
+
+print(yildiz.join(ayrik))
+
+#Eleman Değiştirme: replace(), str.maketrans(), translate()
+
+isim = "Kenan"
+
+print(isim.replace("K","c"))
+
+ifade = "bu ifadede bağzı türkçe karakterler vardır."
+
+duzeltilecek_harfler = "çÇğĞiİüÜ"
+duzeltilmis_harfler = "cCgGiİuU"
+
+alfabe_duzelt = str.maketrans(duzeltilecek_harfler,duzeltilmis_harfler)
+ifade.translate(alfabe_duzelt)
+
+ifade = "Fırat Üniversitesi Yazılım Mühendisliği Programı Veri Madenciliği Dersi Kapsamında Yapılan Çalışmalar"
+
+duzelticekler = "ıİüÜöÖşŞğĞçÇ"
+duzeltilenler = "iIuUoOsSgGcC"
+
+duzelt = ifade.maketrans(duzelticekler,duzeltilenler)
+print(ifade)
+print(ifade.translate(duzelt))
+
+#Contains
+
+isimler = ["ali","ayşe","fatma","nergis","burak","cem"]
+seri = pd.Series(isimler)
+
+print(seri)
+
+print(seri.str.contains("al"))
+
+print(seri[seri.str.contains("al")])
+
+print(seri.str.contains("al").sum())
+
+print(seri.str.contains("[aA]l").sum())
